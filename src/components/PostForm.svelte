3ab07c59bf1c9ad26d6e45e641cbc01752b055c6
JCamayo <jcamayo@idonia.com>
2019-09-27 14:28:51 +0200

    added form and post functionality to submit

---
+++ src/components/PostForm.svelte
@@ -0,0 +1,40 @@
+<script>
+  let title = ''
+  let body = ''
+  const apiBaseUrl = 'https://ndb99xkpdk.execute-api.eu-west-2.amazonaws.com/dev'
+
+  async function onSubmit(event){
+    event.preventDefault()
+    if(title.trim() === '' || body.trim() === '') {
+      return
+    }
+    const newPost = {title, body}
+    const res = await fetch(`${apiBaseUrl}/post`,{
+      method: 'POST',
+      body: JSON.stringify(newPost)
+    })
+    const post = await res.json()
+  }
+</script>
+<div class="w-full">
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" on:submit={onSubmit}>
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="title">
+        Title
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="title" type="text" bind:value={title} placeholder="Title">
+    </div>
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="body">
+        Body
+      </label>
+      <textarea class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="body" type="text" bind:value={body} placeholder="Body"></textarea>
+      <!--<p class="text-red-500 text-xs italic">Please choose a password.</p>-->
+    </div>
+    <div class="flex items-center justify-between">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+        Post
+      </button>
+    </div>
+  </form>
+</div>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,11 +1,19 @@
  <script>
  import { onMount } from 'svelte'
+ import PostForm from '../components/PostForm.svelte'
+
  const apiBaseUrl = 'https://ndb99xkpdk.execute-api.eu-west-2.amazonaws.com/dev'
  let posts = []
  onMount(async () => {
    const res = await fetch(apiBaseUrl + '/posts')
    posts = await res.json()
  })
+ function editPost(post) {
+   console.log(post)
+ }
+ function deletePost(id) {
+   console.log('Deleting post with id:', id)
+ }
 </script>
 
 {#if posts.length === 0}
@@ -16,7 +24,8 @@
 {:else}
 <div class="container flex flex-col justify-between min-h-screen min-w-full bg-white text-gray-800 leading-normal">
   <main class="w-full w-full container max-w-xl mx-auto p-6">
-{#each posts as post}
+  <PostForm />
+  {#each posts as post}
   <div class="max-w-sm max-w-full lg:flex shadow-inner bg-gray-300 border border-gray-400 rounded-b">
         <div class="p-4 flex flex-col justify-between leading-normal">
           <div class="mb-3">
@@ -26,10 +35,21 @@
           <div>
             <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">{post.createdAt}</span>
           </div>
+          <div class="inline-flex mt-4">
+            <button on:click="{() => editPost(post)}" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
+              Edit
+            </button>
+            <button on:click="{() => deletePost(post.id)}" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r">
+              Delete
+            </button>
+          </div>
         </div>
       </div>
       <br>
     {/each}
+    <p class="text-center text-gray-500 text-xs">
+        &copy;2019 Reboseitor Corp. All rights reserved.
+      </p>
 </main>
 </div>
 {/if}
